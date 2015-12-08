669c461c64b0affd66cc5fc09c93a4c38feef5cc
abgbretthaub <abgbretthaub@g02.tfs.local>
2015-12-08 13:19:33 +0000

    startet Versioning with 1.0.0.0
    git-tfs-id: [https://g02deas01tfoun.g02.fujitsu.local/tfs/tpc1]$/GABI_ACPI_DRIVER;C60520

--- _DEV/Source/_SHARED/inc/CommonVersion.h
+++ _DEV/Source/_SHARED/inc/CommonVersion.h
@@ -2,8 +2,8 @@
 #define VER_PRODUCTVERSION 6,70,0107,0
 #define VER_COMPANYNAME_STR "Fujitsu Technology Solutions\0"
 #define VER_LEGALCOPYRIGHT_STR "(c) 2015 Fujitsu Technology Solutions\0"
-#define VER_PRODUCTNAME_STR "DVNC\0"
-#define VER_PRODUCTVERSION_STR "6.70.0107.0\0"
+#define VER_PRODUCTNAME_STR "GABI_ACPI\0"
+#define VER_PRODUCTVERSION_STR "1.00.000.0\0"
 #ifdef _WIN64
 #define VER_PLATFORMAPPEND " (64 bit)"
 #else

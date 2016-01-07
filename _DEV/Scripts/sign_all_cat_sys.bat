104df57d0c19275092b7e5b02889d99d76244313
wannerdext <wannerdext@g02.tfs.local>
2016-01-07 12:51:00 +0000

    [Signierung] XML verändert damit auch EXE-Dateien editiert werden, BAT verändert damit neue Konfigurationsdateien genutzt werden.
    git-tfs-id: [https://g02deas01tfoun.g02.fujitsu.local/tfs/tpc1]$/GABI_ACPI_DRIVER;C60722

--- _DEV/Scripts/sign_all_cat_sys.bat
+++ _DEV/Scripts/sign_all_cat_sys.bat
@@ -5,10 +5,10 @@ if "%~1"=="" goto ERROR
 set _BINARIES_ROOT_="%~1"
 set _LOGFILEPATH_=%_BINARIES_ROOT_%\logs\signlog.txt
 
-"%_BUILDTOOLS_%\signtoolwrapper.exe" /basedir "%_BINARIES_ROOT_%" /inpfile ".\Scripts\sign_all_sys.xml" /logfile %_LOGFILEPATH_% /cfgfile "%_BUILDTOOLS_%\ss_xauth_StWCfg.xml"
-if not "%ERRORLEVEL%"=="0" goto ERROR
+rem "%_BUILDTOOLS_%\signtoolwrapper.exe" /basedir "%_BINARIES_ROOT_%" /inpfile ".\Scripts\sign_all_sys.xml" /logfile %_LOGFILEPATH_% /cfgfile "%_BUILDTOOLS_%\ss_xevauth_StWCfg.xml"
+rem if not "%ERRORLEVEL%"=="0" goto ERROR
 
-"%_BUILDTOOLS_%\signtoolwrapper.exe" /basedir "%_BINARIES_ROOT_%" /inpfile ".\Scripts\sign_all_cat.xml" /logfile %_LOGFILEPATH_% /cfgfile "%_BUILDTOOLS_%\ss_auth_StWCfg.xml"
+"%_BUILDTOOLS_%\signtoolwrapper.exe" /basedir "%_BINARIES_ROOT_%" /inpfile ".\Scripts\sign_all_cat.xml" /logfile %_LOGFILEPATH_% /cfgfile "%_BUILDTOOLS_%\ss_evauth_StWCfg.xml"
 if not "%ERRORLEVEL%"=="0" goto ERROR
 
 goto :END

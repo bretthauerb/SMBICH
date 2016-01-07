c344b09ece170352b59214e8a6a8b99df2d41ff2
wannerdext <wannerdext@g02.tfs.local>
2016-01-07 12:57:12 +0000

    [Signierung] BAT verändert somit werden nun auch die sys-Files signiert.
    git-tfs-id: [https://g02deas01tfoun.g02.fujitsu.local/tfs/tpc1]$/GABI_ACPI_DRIVER;C60725

--- _DEV/Scripts/sign_all_cat_sys.bat
+++ _DEV/Scripts/sign_all_cat_sys.bat
@@ -5,8 +5,8 @@ if "%~1"=="" goto ERROR
 set _BINARIES_ROOT_="%~1"
 set _LOGFILEPATH_=%_BINARIES_ROOT_%\logs\signlog.txt
 
-rem "%_BUILDTOOLS_%\signtoolwrapper.exe" /basedir "%_BINARIES_ROOT_%" /inpfile ".\Scripts\sign_all_sys.xml" /logfile %_LOGFILEPATH_% /cfgfile "%_BUILDTOOLS_%\ss_xevauth_StWCfg.xml"
-rem if not "%ERRORLEVEL%"=="0" goto ERROR
+"%_BUILDTOOLS_%\signtoolwrapper.exe" /basedir "%_BINARIES_ROOT_%" /inpfile ".\Scripts\sign_all_sys.xml" /logfile %_LOGFILEPATH_% /cfgfile "%_BUILDTOOLS_%\ss_xevauth_StWCfg.xml"
+if not "%ERRORLEVEL%"=="0" goto ERROR
 
 "%_BUILDTOOLS_%\signtoolwrapper.exe" /basedir "%_BINARIES_ROOT_%" /inpfile ".\Scripts\sign_all_cat.xml" /logfile %_LOGFILEPATH_% /cfgfile "%_BUILDTOOLS_%\ss_evauth_StWCfg.xml"
 if not "%ERRORLEVEL%"=="0" goto ERROR

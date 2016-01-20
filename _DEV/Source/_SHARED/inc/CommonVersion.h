a1a2075a7ffd74b4e0555aa24d2ad6b1dcf0e582
abgbretthaub <abgbretthaub@g02.tfs.local>
2016-01-20 16:31:20 +0000

    Versionierung implementiert
    git-tfs-id: [https://g02deas01tfoun.g02.fujitsu.local/tfs/tpc1]$/GABI_ACPI_DRIVER;C60930

--- _DEV/Source/_SHARED/inc/CommonVersion.h
+++ _DEV/Source/_SHARED/inc/CommonVersion.h
@@ -1,9 +1,9 @@
-#define VER_FILESERIALVERSION 0107
-#define VER_PRODUCTVERSION 6,70,0107,0
+#define VER_FILESERIALVERSION 0000
+#define VER_PRODUCTVERSION 7,00,0000,0
 #define VER_COMPANYNAME_STR "Fujitsu Technology Solutions\0"
-#define VER_LEGALCOPYRIGHT_STR "(c) 2015 Fujitsu Technology Solutions\0"
+#define VER_LEGALCOPYRIGHT_STR "(c) 2016 Fujitsu Technology Solutions\0"
 #define VER_PRODUCTNAME_STR "GABI_ACPI\0"
-#define VER_PRODUCTVERSION_STR "1.00.000.0\0"
+#define VER_PRODUCTVERSION_STR "7.00.0000.0\0"
 #ifdef _WIN64
 #define VER_PLATFORMAPPEND " (64 bit)"
 #else

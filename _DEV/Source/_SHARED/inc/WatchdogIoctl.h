072a7f340bad8c84c918ae9bcedc7afcd48f1248
abggeberta <abggeberta@g02.tfs.local>
2017-02-07 08:44:22 +0000

    defines Watchdog IOCTLs and data structures for Watchdog in BMC Charites
    git-tfs-id: [https://g02deas01tfoun.g02.fujitsu.local/tfs/tpc1]$/GABI_ACPI_DRIVER;C64946

---
+++ _DEV/Source/_SHARED/inc/WatchdogIoctl.h
@@ -0,0 +1,15 @@
+
+#define IOCTL_WATCHDOG_READCONFIG_REG \
+	CTL_CODE( FILE_DEVICE_UNKNOWN, 0x901, METHOD_BUFFERED,FILE_ANY_ACCESS )
+
+#define IOCTL_WATCHDOG_WRITECONFIG_REG \
+	CTL_CODE( FILE_DEVICE_UNKNOWN, 0x902, METHOD_BUFFERED,FILE_ANY_ACCESS )
+
+// That is how the driver was compiled
+#pragma pack(push,4)
+typedef struct
+{
+	unsigned char	ConfigRegisterAdr;
+	unsigned char	Data;
+} WATCHDOG_INFO, *WATCHDOG_INFO;
+#pragma pack(pop)

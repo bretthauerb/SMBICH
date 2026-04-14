744675f1e32712b3e4be4cadb2eda13309ce65ce
Birgit Bretthauer <BretthauerB@dev.fccldev.de>
2026-04-14 11:28:30 +0200

    changed branding in commonversion.h

--- _DEV/Source/_SHARED/inc/CommonVersion.h
+++ _DEV/Source/_SHARED/inc/CommonVersion.h
@@ -1,9 +1,9 @@
 #define VER_FILESERIALVERSION 0000
-#define VER_PRODUCTVERSION 7,00,0000,0
-#define VER_COMPANYNAME_STR "Fujitsu Client Computing Limited\0"
-#define VER_LEGALCOPYRIGHT_STR "(c) 2025 Fujitsu Client Computing Limited\0"
+#define VER_PRODUCTVERSION 8,5,0000,0
+#define VER_COMPANYNAME_STR "Kontron Europe GmbH\0"
+#define VER_LEGALCOPYRIGHT_STR "(c) 2026 Kontron Europe GmbH\0"
 #define VER_PRODUCTNAME_STR "GABI_ACPI\0"
-#define VER_PRODUCTVERSION_STR "7.00.0000.0\0"
+#define VER_PRODUCTVERSION_STR "8.5.0000.0\0"
 #ifdef _WIN64
 #define VER_PLATFORMAPPEND " (64 bit)"
 #else
